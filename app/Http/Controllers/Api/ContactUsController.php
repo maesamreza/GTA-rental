1b54720545d46c8bc49ff3ee1e3b407d3453faab
hassanqazi146 <hassanqazi146@gmail.com>
2022-10-19 15:46:29 +0500

    contact us changes

--- app/Http/Controllers/Api/ContactUsController.php
+++ app/Http/Controllers/Api/ContactUsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\{Property};
 use Validator;
 use Mail;
 
@@ -20,18 +21,24 @@ class ContactUsController extends Controller
             "price" => "required",
             "property_type" => "required|max:200",
             "message" => "nullable|max:5000",
+            "property_id"=>"required"
             //"file" => "nullable",
         );
         $validator = Validator::make($controlls, $rules);
         if ($validator->fails()) {
             return response()->json(['status' => false, 'message' => 'Inputs Errors', 'errors' => $validator->errors()]);
         }
-        $email="hassanqazi146@gmail.com";
+        $property=Property::with('user')->where("id",$request->property_id)->first();
+        if(!empty($property->user)){
+            $email=$property->user->email;
+        }else{
+          $email="hassanqazi146@gmail.com";
+        }
         Mail::send('email_template.contact_us',['contact_info'=>$controlls],function ($message) use ($email) {
             $message->from(env('MAIL_FROM_ADDRESS'),'Gta Rental');
             $message->to($email);
             //$message->to('submit@newontariohomes.ca');
-            //$message->to('samarhussain1@gmail.com');
+            $message->to('samarhussain1@gmail.com');
             $message->subject('Contact Email');
         });
         return response(["status"=>true,"success" => "Contact Us Email Send Successfully"], 200);
